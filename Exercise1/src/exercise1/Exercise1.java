745e238c80abdc36a849ea2c3774cfa648430d41
MinhPhuong <mp753114@gmail.com>
2021-07-14 17:41:19 +0700

    first commit

---
+++ Exercise1/src/exercise1/Exercise1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise1;
+
+import java.sql.SQLException;
+import layout.MainLayout;
+
+/**
+ *
+ * @author Thang
+ */
+public class Exercise1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        MainLayout main = new MainLayout();
+        main.show();
+    }
+    
+}
